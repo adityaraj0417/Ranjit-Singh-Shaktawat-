e1210bcf4889f1e3443491585c48a184f6cf55ee
Ranjit Singh Shaktawat <114395990+adityaraj0417@users.noreply.github.com>
2024-09-16 14:44:19 +0530

    Add files via upload

---
+++ Day 11/index.js
@@ -0,0 +1,27 @@
+// let n = 8; // Number of rows
+
+// for (let i = 1; i <= n; i++) {
+//     let str = '';
+//     for (let j = 1; j <= i; j++) {
+//         str += '* ';
+//     }
+//     console.log(str);
+// }
+let n = 8; // Number of rows
+
+for (let i = n; i >= 1; i--) {
+    let str = '';
+    for (let j = 1; j <= i; j++) {
+        str += '* ';
+    }
+    console.log(str);
+}
+// let n = 5; // Number of rows
+
+// for (let i = 1; i <= n; i++) {
+//     let str = '';
+//     for (let j = 1; j <= i; j++) {
+//         str += '* ';
+//     }
+//     console.log(str);
+// }
