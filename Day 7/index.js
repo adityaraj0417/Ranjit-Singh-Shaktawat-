cb0c65f138cdd99a9b7267799ef9263405c266d7
Ranjit Singh Shaktawat <114395990+adityaraj0417@users.noreply.github.com>
2024-09-16 14:43:00 +0530

    Add files via upload

---
+++ Day 7/index.js
@@ -0,0 +1,12 @@
+console.log("git version 1");
+console.log("git version 1 new line")
+
+
+//git init
+//git status
+// git add
+//git commit 'msg'
+//git log--all
+//git config --global user.email "adityarajsinghshaktawat96@gmail.com"
+// git config --global user.name "Ranjit singh"
+//git commit -m "initialcommit"
